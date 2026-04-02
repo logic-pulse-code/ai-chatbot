975dfd83993a871bceb5c5e6eeeba041aff83256
logic-pulse-code <mainaffiliateprogram@gmail.com>
2026-04-02 13:05:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # In Produktion hier deine Domain eintragen
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
